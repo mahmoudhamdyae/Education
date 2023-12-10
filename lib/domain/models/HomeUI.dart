a8201dd445ae7ba50cf2023197aa003f16917923
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-12-10 16:49:57 +0200

    Home Screen Model

--- lib/domain/models/HomeUI.dart
+++ lib/domain/models/HomeUI.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../presentation/resources/routes_manager.dart';
+import '../../presentation/resources/strings_manager.dart';
 
 class HomeUI {
   String name;
@@ -6,4 +9,40 @@ class HomeUI {
   Function action;
 
   HomeUI(this.name, this.icon, this.action);
+
+  static getItems(BuildContext context) {
+    return [
+      HomeUI(
+        AppStrings.recordedCourses,
+        Icons.person,
+            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.recordedCoursesRoute),
+      ),
+      HomeUI(
+        AppStrings.printedNotes,
+        Icons.person,
+            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.printedNotesRoute),
+      ),
+      HomeUI(
+        AppStrings.courses,
+        Icons.person,
+            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.coursesRoute),
+      ),
+      HomeUI(
+        AppStrings.onlineCourses,
+        Icons.person,
+            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.onlineCoursesRoute),
+      ),
+      HomeUI(
+        AppStrings.teacher,
+        Icons.person,
+            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.teacherRoute),
+      ),
+      HomeUI(
+        AppStrings.examsAndBanks,
+        Icons.person,
+            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.examsAndBanksRoute),
+      ),
+
+    ];
+  }
 }
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,6 @@
-import 'package:education/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/models/HomeUI.dart';
-import '../../resources/strings_manager.dart';
 import '../../widgets/custom_grid_view.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,40 +8,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final List<HomeUI> items = [
-      HomeUI(
-        AppStrings.recordedCourses,
-        Icons.person,
-            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.recordedCoursesRoute),
-      ),
-      HomeUI(
-        AppStrings.printedNotes,
-        Icons.person,
-            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.printedNotesRoute),
-      ),
-      HomeUI(
-        AppStrings.courses,
-        Icons.person,
-            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.coursesRoute),
-      ),
-      HomeUI(
-        AppStrings.onlineCourses,
-        Icons.person,
-            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.onlineCoursesRoute),
-      ),
-      HomeUI(
-        AppStrings.teacher,
-        Icons.person,
-            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.teacherRoute),
-      ),
-      HomeUI(
-        AppStrings.examsAndBanks,
-        Icons.person,
-            () => Navigator.of(context, rootNavigator: true).pushNamed(Routes.examsAndBanksRoute),
-      ),
-
-    ];
-    return CustomGridView(items);
+    return CustomGridView(HomeUI.getItems(context));
   }
 }
