96a8d6e6df1367a7f291a76854ceb9127abbce53
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-01-15 11:27:30 +0200

    Lesson Screen

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -7,6 +7,7 @@ class ColorManager {
   static const Color white = Color(0xffffffff);
   static const Color black = Color(0xff000000);
   static const Color blue = Color(0xff0000ff);
+  static const Color grey = Color(0xff808080);
   static const Color fabBackground = Color(0xff2b92da);
   static const Color error = Color(0xffff0000);
 }
--- lib/presentation/screens/lesson/widgets/tab_course_lessons.dart
+++ lib/presentation/screens/lesson/widgets/tab_course_lessons.dart
@@ -1,21 +1,60 @@
-import 'package:flutter/cupertino.dart';
+import 'package:education/presentation/resources/color_manager.dart';
+import 'package:flutter/material.dart';
 
 import '../../../../domain/models/lesson/wehda.dart';
-import 'course_lessons_widget.dart';
 
 class TabCourseLessons extends StatelessWidget {
 
   final List<Wehda> wehdat;
   final bool isInLessonScreen;
 
-  const TabCourseLessons({super.key, required this.wehdat, required this.isInLessonScreen});
+  final lessons = List.generate(10, (index) => 'اسم الدرس');
+
+  TabCourseLessons({super.key, required this.wehdat, required this.isInLessonScreen});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: CourseLessonWidget(
-        wehdat: wehdat,
-        isInLessonScreen: isInLessonScreen,
+      child: ListView.separated(
+          itemCount: lessons.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: ColorManager.secondary, width: 1)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Text(
+                        index < 9 ? '0${index + 1}' : '${index + 1}',
+                        style: const TextStyle(
+                            color: ColorManager.secondary
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8.0,),
+                  Text(
+                    lessons[index],
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Container()
+                ],
+              ),
+            );
+          }, separatorBuilder: (BuildContext context, int index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+              child: Divider(color: ColorManager.grey, thickness: 0.08,),
+            );
+      },
       ),
     );
   }
