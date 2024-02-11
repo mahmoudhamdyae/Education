9c3bd8bef53e47025448eaa88101897425520dca
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-02-11 18:48:46 +0200

    Some Fixes

--- lib/presentation/widgets/courses_list.dart
+++ lib/presentation/widgets/courses_list.dart
@@ -1,5 +1,4 @@
 import 'package:education/presentation/screens/course/widgets/course_screen.dart';
-import 'package:education/presentation/widgets/price_widget.dart';
 import 'package:education/presentation/widgets/rate_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,7 +7,6 @@ import '../../domain/models/courses/course.dart';
 import '../resources/assets_manager.dart';
 import '../resources/color_manager.dart';
 import '../resources/font_manager.dart';
-import '../resources/strings_manager.dart';
 import '../resources/styles_manager.dart';
 import 'bookmark_course.dart';
 import 'buy_widget.dart';
@@ -65,13 +63,7 @@ class CoursesList extends StatelessWidget {
                             style: getSmallStyle(fontSize: 13),
                           ),
                           const SizedBox(height: 16.0,),
-                          Row(
-                            children: [
-                              RateWidget(rate: courses[index].rate),
-                              const SizedBox(width: 12.0,),
-                              PriceWidget(price: courses[index].term, month: AppStrings.monthly,),
-                            ],
-                          )
+                          RateWidget(rate: courses[index].rate),
                         ],
                       ),
                       Expanded(child: Container()),
