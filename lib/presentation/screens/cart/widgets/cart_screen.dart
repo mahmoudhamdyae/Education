e14ee6f3b78b5c9b1c767d0b56f0a71b7e191669
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-02-14 14:24:51 +0200

    Cart Screen redesign

--- lib/presentation/screens/cart/widgets/cart_screen.dart
+++ lib/presentation/screens/cart/widgets/cart_screen.dart
@@ -9,6 +9,7 @@ import 'package:education/presentation/screens/cart/widgets/finish_order_screen.
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../core/utils/insets.dart';
 import '../../../../domain/models/notes/note.dart';
 import '../../../widgets/error_screen.dart';
 import '../../../widgets/loading_screen.dart';
@@ -39,96 +40,143 @@ class CartScreen extends StatelessWidget {
               } else if (controller.cartNotes.isEmpty && controller.cartPackages.isEmpty){
                 return const EmptyCart(emptyString: AppStrings.noCart);
               }
-              return ListView(
-                shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 8.0),
-                    child: Column(
-                      children: [
-                        // الإجمالى
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              AppStrings.sum,
-                              style: getLargeStyle(),
-                            ),
-                            Text(
-                              '${controller.sum} د.ك',
-                              style: getSmallStyle(),
-                            ),
-                          ],
-                        ),
-                        // الخصم
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              AppStrings.discount,
-                              style: getLargeStyle(),
-                            ),
-                            Text(
-                              '- ${controller.discount} د.ك',
-                              style: getSmallStyle(),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8.0,),
-                        const Divider(height: 1, color: ColorManager.primaryTransparent,),
-                        const SizedBox(height: 8.0,),
-                        // المحموع الكلى
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              AppStrings.total,
-                              style: getLargeStyle(),
-                            ),
-                            Text(
-                              '${controller.totalSum} د.ك',
-                              style: getSmallStyle(),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  // اتمام عملية الشراء
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FilledButton(
-                      style: getFilledButtonStyle(),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                          return AlertDialog(
-                            backgroundColor: ColorManager.white,
-                            // shape: RoundedRectangleBorder(
-                            //   borderRadius: BorderRadius.all(Radius.circular(AppSize.s20)),
-                            // ),
-                            title: Text(
-                              AppStrings.orderDetails,
-                              style: getLargeStyle(),
-                            ),
-                            content: const FinishOrderScreen(),
-                          );
-                        });
-                      },
-                      child: const Text(AppStrings.finishOrder),
-                    ),
-                  ),
-                  controller.cartPackages.isEmpty ? Container() : _buildPackagesList(packages),
-                  controller.cartNotes.isEmpty ? Container() : _buildNotesList(notes),
-                ],
-              );
+              return isWide(context) ?
+              _buildTwoColumn(context, controller, packages, notes)
+                  :
+              _buildOneColumn(context, controller, packages, notes);
             },
           )
         ],
       ),
     );
+  }
+
+  Widget _buildOneColumn(BuildContext context, PrintedNotesController controller, List<Package> packages, List<Note> notes) {
+    return ListView(
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      children: [
+        _buildHeader(controller),
+        // اتمام عملية الشراء
+        _buildBuyButton(context),
+        _buildBooksAndPackages(controller, packages, notes),
+      ],
+    );
+  }
+
+  Widget _buildTwoColumn(BuildContext context, PrintedNotesController controller, List<Package> packages, List<Note> notes) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 2,
+          child: ListView(
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            children: [
+              _buildHeader(controller),
+              // اتمام عملية الشراء
+              _buildBuyButton(context),
+            ],
+          ),
+        ),
+        const SizedBox(width: 16.0,),
+        Expanded(
+            flex: 3,
+            child: _buildBooksAndPackages(controller, packages, notes),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBooksAndPackages(PrintedNotesController controller, List<Package> packages, List<Note> notes) {
+    return ListView(
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      children: [
+        controller.cartPackages.isEmpty ? Container() : _buildPackagesList(packages),
+        controller.cartNotes.isEmpty ? Container() : _buildNotesList(notes),
+      ],
+    );
+  }
+
+  Padding _buildBuyButton(BuildContext context) {
+    return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FilledButton(
+                    style: getFilledButtonStyle(),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                        return AlertDialog(
+                          backgroundColor: ColorManager.white,
+                          title: Text(
+                            AppStrings.orderDetails,
+                            style: getLargeStyle(),
+                          ),
+                          content: const FinishOrderScreen(),
+                        );
+                      });
+                    },
+                    child: const Text(AppStrings.finishOrder),
+                  ),
+                );
+  }
+
+  Padding _buildHeader(PrintedNotesController controller) {
+    return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 8.0),
+                  child: Column(
+                    children: [
+                      // الإجمالى
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            AppStrings.sum,
+                            style: getLargeStyle(),
+                          ),
+                          Text(
+                            '${controller.sum} د.ك',
+                            style: getSmallStyle(),
+                          ),
+                        ],
+                      ),
+                      // الخصم
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            AppStrings.discount,
+                            style: getLargeStyle(),
+                          ),
+                          Text(
+                            '- ${controller.discount} د.ك',
+                            style: getSmallStyle(),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8.0,),
+                      const Divider(height: 1, color: ColorManager.primaryTransparent,),
+                      const SizedBox(height: 8.0,),
+                      // المحموع الكلى
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            AppStrings.total,
+                            style: getLargeStyle(),
+                          ),
+                          Text(
+                            '${controller.totalSum} د.ك',
+                            style: getSmallStyle(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
   }
 
   Widget _buildPackagesList(List<Package> packages) {
